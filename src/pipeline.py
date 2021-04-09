85dffa0b3c65210f63d6e5532b8b7e8eba13c112
dknorr <dknorr01@gmail.com>
2021-04-09 13:39:11 -0400

    got csv pulled in

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,14 @@
-print("Hello Docker")
+import csv
+import requests
+
+FREIGHT_CSV_URL = "https://data.transportation.gov/api/views/uxue-t623/rows.csv?accessType=DOWNLOAD"
+
+with requests.Session() as s:
+    download = s.get(FREIGHT_CSV_URL)
+
+    decoded_content = download.content.decode('utf-8')
+
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
